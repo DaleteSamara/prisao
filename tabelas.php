3cd4da3872398f803c7a569946b616004d194de5
gihmartinez <37456353+gihmartinez@users.noreply.github.com>
2018-06-22 08:41:13 -0300

    Delete tabelas.php

--- tabelas.php
+++
@@ -1,7 +0,0 @@
-<div class="a">
-  <p><a href="exibe_cidade_estado.php">Tabela cidades e estados</a><p/>
-  <p><a href="exibe_detento.php">Tabela detento</a><p/>
-  <p><a href="exibe_prisao.php">Tabela prisao</a><p/>
-  <p><a href="exibe_detento_prisao.php">Tabela detento e prisao</a><p/>
-</div>
-  
